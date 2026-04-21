833a08fe8e1886df6ae535e7e20e4a3a51b50fc6
Jojo Java <jsjava.cvt@.eac.edu.ph>
2026-04-21 13:49:47 +0800

    Added new images in gallery data

--- src/constants/galleryData.js
+++ src/constants/galleryData.js
@@ -1,10 +1,22 @@
 // Gallery Images
 import gallery2 from "../assets/img/gallery2.jpg";
 import gallery3 from "../assets/img/gallery3.jpg";
-import gallery5 from "../assets/img/gallery5.jpg";
 import gallery7 from "../assets/img/gallery7.jpg";
 import gallery11 from "../assets/img/gallery11.jpg";
 import hero from "../assets/img/hero.jpg";
+import pool1 from "../assets/img/pool-1.jpg";
+import pool2 from "../assets/img/pool-2.jpg";
+import pool3 from "../assets/img/pool-3.jpg";
+import pool4 from "../assets/img/pool-4.jpg";
+import pool5 from "../assets/img/pool-5.jpg";
+import kitchen from "../assets/img/kitchen.jpg";
+import living1 from "../assets/img/living-1.jpg";
+import living2 from "../assets/img/living-2.jpg";
+import living3 from "../assets/img/living-3.jpg";
+import living4 from "../assets/img/living-4.jpg";
+import living5 from "../assets/img/living-5.jpg";
+import living6 from "../assets/img/living-6.jpg";
+import living7 from "../assets/img/living-7.jpg";
 
 export const galleryImages = [
   {
@@ -18,9 +30,29 @@ export const galleryImages = [
     caption: "Beautiful Scenery",
   },
   {
-    src: gallery5,
-    alt: "Surf-ready beach",
-    caption: "Surf-ready beach",
+    src: pool1,
+    alt: "Pool View",
+    caption: "Pool View",
+  },
+  {
+    src: pool2,
+    alt: "Pool View",
+    caption: "Pool View",
+  },
+  {
+    src: pool3,
+    alt: "Pool View",
+    caption: "Pool View",
+  },
+  {
+    src: pool4,
+    alt: "Pool View",
+    caption: "Pool View",
+  },
+  {
+    src: pool5,
+    alt: "Pool View",
+    caption: "Pool View",
   },
   {
     src: gallery11,
@@ -32,5 +64,45 @@ export const galleryImages = [
     src: gallery7,
     alt: "Spacious living room",
     caption: "Spacious living room",
+  },
+  {
+    src: kitchen,
+    alt: "Modern Kitchen Space",
+    caption: "Modern Kitchen Space",
+  },
+  {
+    src: living1,
+    alt: "Cozy Living Area",
+    caption: "Cozy Living Area",
+  },
+  {
+    src: living2,
+    alt: "Spacious Living Room",
+    caption: "Spacious Living Room",
+  },
+  {
+    src: living3,
+    alt: "Comfortable Seating Area",
+    caption: "Comfortable Seating Area",
+  },
+  {
+    src: living4,
+    alt: "Bright and Airy Living Space",
+    caption: "Bright and Airy Living Space",
+  },
+  {
+    src: living5,
+    alt: "Modern Living Room Design",
+    caption: "Modern Living Room Design",
+  },
+  {
+    src: living6,
+    alt: "Lounge and Entertainment Area",
+    caption: "Lounge and Entertainment Area",
+  },
+  {
+    src: living7,
+    alt: "Living Room Overview",
+    caption: "Living Room Overview",
   },
 ];
